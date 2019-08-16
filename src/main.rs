d3fcaabaa5abd322b98c6c84944e1c66e7fff3cd
András Veres-Szentkirályi <vsza@vsza.hu>
2019-08-16 16:36:43 +0200

    removed unnecessary Buf{Reader,Writer}

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate image;
 
 use std::io;
 use std::io::{Read,Write};
-use std::io::{BufReader, BufWriter};
 use std::sync::{Arc, Mutex,MutexGuard};
 use std::thread;
 
@@ -22,9 +21,9 @@ struct Crop {
 
 fn main() {
     let stdin = io::stdin();
-    let mut br = BufReader::new(stdin.lock());
+    let mut sil = stdin.lock();
     let stdout = io::stdout();
-    let mut bw = BufWriter::new(stdout.lock());
+    let mut sol = stdout.lock();
     let mut frame: [u8; BYTES_PER_FRAME] = [0; BYTES_PER_FRAME];
 
     let crop = Arc::new(Mutex::new(Crop { x: 0, y: 0, w: WIDTH as u16, h: HEIGHT as u16 }));
@@ -44,7 +43,7 @@ fn main() {
     });
 
     loop {
-        match br.read_exact(& mut frame) {
+        match sil.read_exact(& mut frame) {
             Err(e) => match e.kind() {
                 io::ErrorKind::UnexpectedEof => return (),
                 _ => panic!("Can't read frame: {}", e),
@@ -57,7 +56,7 @@ fn main() {
         let resized = cropped.resize_exact(WIDTH as u32, HEIGHT as u32, FilterType::CatmullRom);
         let resized8 = resized.as_rgb8().expect("Cannot convert to RGB8");
 
-        match bw.write_all(&resized8) {
+        match sol.write_all(&resized8) {
             Err(e) => match e.kind() {
                 io::ErrorKind::BrokenPipe => return (),
                 _ => panic!("Can't write frame: {}", e),
